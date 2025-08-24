bf626d6cedad87926cd0207cc9156c889cd1d121
Artem Kril <arturkril9@gmail.com>
2025-08-24 15:16:27 +0300

    2nd Lesson. Object and methods

--- JS/main.js
+++ JS/main.js
@@ -73,4 +73,78 @@
 // console.log(item);
 // for (let key in item){
 //     console.log(key, item[key]);
+// }          
+
+
+
+// HM2
+// 1
+// Якщо змінна більше нуля - виведіть true, менше - false
+// // Перевірте це на варіантах 1, 0, -3.
+// let a = prompt("Введіть число")
+// a > 0 ? console.log(true) : console.log(false);
+
+// 2
+// Якщо змінна ="test" - виведіть true,
+// Перевірте це на варіантах 'test',"qwerty", true
+// let a = prompt("Введіть текст");
+// a === "test" ? console.log(true) : console.log(false);
+
+// 3
+// Якщо змінна більше 10 - відніміть 5,
+// менше - додайте 5, результат виведіть в консоль
+// Перевірте це на варіантах 1, 10, 13.
+// let a = prompt('Введіть число');
+// let num = Number(a);
+// if (num > 10){
+//     console.log(num - 5);
+// } else if (num < 10){
+//     console.log(num+5);
+// } else {
+//     console.log("Число або рівне 10, або це не число");
 // }
+
+// 4
+// /Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+// let num = prompt("Введіть число місяця та отримаєте місяць");
+// let month = Number(num);
+// switch (month){
+//     case 1: alert('Січень');
+//     break;
+//     case 2: alert('Лютий');
+//     break;
+//     case 3: alert('Березень');
+//     break;
+//     case 4: alert('Квітень');
+//     break;
+//     case 5: alert('Травень');
+//     break;
+//     case 6: alert('Червень');
+//     break;
+//     case 7: alert('Липень');
+//     break;
+//     case 8: alert('Серпень');
+//     break;
+//     case 9: alert('Вересень');
+//     break;
+//     case 10: alert('Жовтень');
+//     break;
+//     case 11: alert('Листопад');
+//     break;
+//     case 12: alert('Грудень');
+//     break;
+//     default: alert("Це не число місяця!")
+// }
+
+// 5
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+// let a = prompt("Введіть будь-яке тризначне число");
+// let digit1 = Number(a[0]);
+// let digit2 = Number(a[1]);
+// let digit3 = Number(a[2]);
+// let sum = digit1 + digit2 + digit3;
+// alert(`Сума ваших чисел = ${sum}`);
+
+
