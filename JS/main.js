d884be156cd225e82ef9cba6bf8fbd5ad77cfad2
Artem Kril <arturkril9@gmail.com>
2025-08-27 14:45:21 +0300

    5th Lesson JS. this, apply, call, bind та замикання

--- JS/main.js
+++ JS/main.js
@@ -253,3 +253,82 @@
 //     alert("Ви скасували виконання.");
 //   }
 // );
+
+// HM5
+// ---1---
+// написати об*єкт студента який буде виводити ім*я, спеціальнісь, середній
+// бал і кількість пропущених занять
+// 2) написати метод який буде виводити цю інформацію
+// 3) написати три варіанти студентів
+// 4) прикріпити знначення за допомогою call apply bind
+// const pupil = {
+//     name: "Ім'я",
+//     faculty: "Факультет",
+//     avgMark: "Середній бал",
+//     aLess: "Пропущено занять",
+//     getInfo: function(){
+//         console.log(`Студент/-ка ${this.name} вчиться на спеціальності ${this.faculty} та має середній бал ${this.avgMark}. Зокрема пропустив/-ла ${this.aLess} уроків`);
+//     }
+// }
+
+// const Andrii = {
+//     name: "Andrii",
+//     faculty: "International Relations",
+//     avgMark: "8,7",
+//     aLess: "35"
+// };
+// const Ruslana = {
+//       name: "Ruslana",
+//     faculty: "International Relations",
+//     avgMark: "10,7",
+//     aLess: "15"
+// };
+// const Artem = {
+//       name: "Artem",
+//     faculty: "International Relations",
+//     avgMark: "11,7",
+//     aLess: "75"
+// };
+//  pupil.getInfo.call(Andrii);
+//  pupil.getInfo.bind(Ruslana)();
+//  pupil.getInfo.apply(Artem);
+
+// // ---2---
+// Написати дві кнопки і закріпити на них функції
+// при натисканні на кнопку html - має видати коротке визначення що це таке
+// при натисканні на кнопку css - має видати коротке визначення що це таке
+
+// const term = {
+//     getTerminology(){
+//         console.log(`${this.term}`);
+//     }
+// };
+
+// const htmlTerm = {
+//     term: "Html - це скелет сайту"
+// }
+// const cssTerm = {
+//     term: "Css - це каскадні стилі до елементів в документі."
+// }
+
+// // const getHtml = document.querySelector("#html").addEventListener("click", );
+// const getCss = document.querySelector("#css").addEventListener("click", term.getTerminology.bind(cssTerm));
+// const getHtml = document.querySelector("#html").addEventListener("click", term.getTerminology.bind(htmlTerm));
+
+// --- 3 ---
+// Написати функцію магазин, яка отримує назву товару, ціну за кг і кількість товару
+// функція має повертати назву товару і вартість
+// перевірити на варіантах:
+// 1) banana 30, 4,5
+// 2) cherry 58, 1,3
+// 3) jrange 89. 3,4
+// const getFruits = function(name, pricePerKilo, quantity){
+    // return `${name} буде коштувати `+ pricePerKilo * quantity + ` грн`;
+    // return {
+    //     name: name,
+    //     amount: pricePerKilo * quantity
+    // }
+// };
+// getFruits("banana", 30, 4.5);
+// console.log(
+// getFruits("banana", 30, 4.5));
